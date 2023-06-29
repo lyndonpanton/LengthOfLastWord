39006d320aaa130c0fa9227143f32da82de73a35
Lyndon Panton <LyndonPanton@gmail.com>
2023-06-29 17:13:52 +0100

    Added test cases

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -3,6 +3,20 @@
 
     public class Solution
     {
+        static void Main(string[] args)
+        {
+            int testCaseOne = LengthOfLastWord("Hello World");
+            // Expected 5
+            Console.WriteLine(testCaseOne);
+
+            int testCaseTwo = LengthOfLastWord("   fly me   to   the moon  ");
+            // Expected 4
+            Console.WriteLine(testCaseTwo);
+
+            int testCaseOThree = LengthOfLastWord("luffy is still joyboy");
+            // Expected 6
+            Console.WriteLine(testCaseOThree);
+        }
 
         public static int LengthOfLastWord(string s)
         {
